dae2e2b2620399d311d7d449d98d48f978345b04
Eduardo Balbinot <eduardo.balbinot@gmail.com>
2014-11-06 15:33:51 +0000

    removed test requirement empty list

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.6',
@@ -46,5 +46,5 @@ setup(
 #        'Programming Language :: Python :: 3.4',
     ],
     test_suite='tests',
-    tests_require=test_requirements
+#    tests_require=test_requirements
 )
