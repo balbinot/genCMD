91e5e485e7df59c1913d034e2c18188c9d474cfe
Eduardo Balbinot <eduardo.balbinot@gmail.com>
2014-11-06 15:23:55 +0000

    trying again tests

--- tests/test_gencmd.py
+++ tests/test_gencmd.py
@@ -8,7 +8,29 @@ test_gencmd
 Tests for `gencmd` module.
 """
 
-import doctest
+import unittest
+import gencmd
 
-from gencmd.df import Kroupa
-from gencmd.model import SSP
+class testgencmd(unittest.TestCase):
+    """ Test class for gencmd """
+
+    def setUp(self):
+
+        self.kroupa_sampler = gencmd.df.Kroupa()
+        pass
+
+    def testKroupa(self):
+        self.assertEqual(len(self.kroupa_sampler.sample(10)), 10)
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(testgencmd))
+    return suite
+
+
+if __name__ == '__main__':
+
+    #unittest.main()
+    suiteFew = unittest.TestSuite()
+    suiteFew.addTest(testgencmd("testKroupa"))
+    unittest.TextTestRunner(verbosity=2).run(suite())
